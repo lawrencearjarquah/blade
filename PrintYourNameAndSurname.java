ad6730d4996f5c85727921802c4e51ca598cc6bc
lawrence <odoyebre@gmail.com>
2023-06-12 16:39:40 +0200

    Input and Out 2, stampa sia nome che il cognome sulla stessa riga separati da uno spazio

---
+++ PrintYourNameAndSurname.java
@@ -0,0 +1,9 @@
+package Esercizio_2;
+
+public class PrintYourNameAndSurname {
+    public static void main(String[] args) {
+        System.out.print("Lawrence ");
+        System.out.print("Arjarquah");
+
+    }
+}
